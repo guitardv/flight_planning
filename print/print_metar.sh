7452f1202e051f7b1ba9cee322ad2ad1c03dcb74
Vincent Guitard <guitard.vincent@protonmail.com>
2023-04-07 02:00:58 -0400

    path correction in print/print_metar.sh

--- print/print_metar.sh
+++ print/print_metar.sh
@@ -6,7 +6,7 @@ if [ $# -gt 0 ] ; then
     for argumentSHcurrent in $@ ; do
 	    if [ $argumentSHcurrent != $0 ] ; then argumentsSH=$argumentsSH' '$argumentSHcurrent ; fi
     done
-    python3 "$SCRIPT_DIR/metar.py" $argumentsSH | python3 "$SCRIPT_DIR/print_metar.py"
+    python3 "$SCRIPT_DIR/../metar/metar.py" $argumentsSH | python3 "$SCRIPT_DIR/print_metar.py"
 else
-    python3 "$SCRIPT_DIR/metar.py" | python3 "$SCRIPT_DIR/print_metar.py"
+    python3 "$SCRIPT_DIR/../metar/metar.py" | python3 "$SCRIPT_DIR/print_metar.py"
 fi
